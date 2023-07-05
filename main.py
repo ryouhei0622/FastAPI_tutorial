e09ee8d66a52576396842494af50a1086205932d
ryouhei0622 <seijakunomutou@gmail.com>
2023-07-05 14:38:41 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("modeks/{modek_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexner:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
