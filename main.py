658ce24649200c20312b868ed0fe9d070e208daa
ryouhei0622 <seijakunomutou@gmail.com>
2023-07-06 12:03:20 +0900

    列挙型の例

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+    example = "sairiumu"
 
 
 app = FastAPI()
@@ -21,3 +22,15 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+# print(ModelName.alexnet.value)
+# a = ModelName("alexnet")
+# print(a is ModelName.alexnet)
+
+print(ModelName["example"])
+print(ModelName["example"].value)
+print(ModelName.example)
+print(ModelName.example.value)
+print(ModelName("sairiumu"))
+print(ModelName("sairiumu").value)
